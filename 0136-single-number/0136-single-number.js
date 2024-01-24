1ce140c7a8fd5bfb4407fd6897cf3fd47bd7a570
Annisa Rachmasari <saa.annisa15@gmail.com>
2024-01-24 20:47:40 +0700

    Update 0136-single-number.js

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -5,9 +5,9 @@
 var singleNumber = function(nums) {
     //create a temp array
     let temp = []
-    //iterate through array, push into temp.
+    //iterate through array
     for(let i=0; i<nums.length; i++) {
-        //find whether in temp array already have the same num or not
+        //find whether the temp array already has the same num or not, if it hasn't, push the num to temp arr
         if(temp.indexOf(nums[i]) == -1) {
             temp.push(nums[i])
         } else {
