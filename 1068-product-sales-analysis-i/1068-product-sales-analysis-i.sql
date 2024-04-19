8b013cfcb5817745e09174bd0538e9ae310a394d
Annisa Rachmasari <saa.annisa15@gmail.com>
2024-04-19 22:23:29 +0700

    Time: 2091 ms (65.08%), Space: 0B (100.00%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -3,5 +3,5 @@ select
     p.product_name,
     s.year,
     s.price
-from Product p
-    right join Sales s on p.product_id = s.product_id
+from Sales s
+    left join Product p on p.product_id = s.product_id
