9991390f4edb0a1f6a8aba208d4156c0b579e958
Annisa Rachmasari <saa.annisa15@gmail.com>
2024-10-16 07:16:28 +0700

    Time: 54 ms (79.24%), Space: 51.4 MB (77.07%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.ts
+++ 0088-merge-sorted-array/0088-merge-sorted-array.ts
@@ -2,9 +2,6 @@
  Do not return anything, modify nums1 in-place instead.
  */
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    //check wether the array has 0 or not
-    //how? looping? then if its not 0, push on a new array
-    //sort the array in ascending order
     let res:number[] = []
     for(let i=0 ; i < m; i++) {
         res.push(nums1[i])
