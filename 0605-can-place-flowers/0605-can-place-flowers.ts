c7957b181a41cb356bb1814c275688a496660e11
Annisa Rachmasari <saa.annisa15@gmail.com>
2024-10-20 11:31:41 +0700

    Time: 2 ms (99.72%), Space: 52.4 MB (28.93%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.ts
@@ -0,0 +1,27 @@
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+    //check is there any empty space after, before, and the current index
+    //if there's, then substract the n with 1
+    if (n == 0) {
+    return true;
+  }
+  for (let i = 0; i < flowerbed.length; i++) {
+    let prev = flowerbed[i - 1] === 0;
+    let next = flowerbed[i + 1] === 0;
+    //check whether the current index is 0
+    if (flowerbed[i] === 0) {
+      //if the first index is 0, and next one is 0, it can be planted
+      //if the prev & next index is 0, it can be planted
+      //if the last index and the prev is zero, it can be planted
+      if (
+        (i === 0 && next) ||
+        (prev && next) ||
+        (i === flowerbed.length - 1 && prev) ||
+        (flowerbed.length == 1)
+      ) {
+        flowerbed[i] = 1;
+        n -= 1;
+      }
+    }
+  }
+  return n <= 0;
+ };
