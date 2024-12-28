5e7ca997b3fb5193d71cb48c95d5716a1797c186
Annisa Rachmasari <saa.annisa15@gmail.com>
2024-12-29 06:35:33 +0700

    Time: 9 ms (89.42%), Space: 63.7 MB (37.01%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.ts
+++ 0217-contains-duplicate/0217-contains-duplicate.ts
@@ -1,9 +1,15 @@
 function containsDuplicate(nums: number[]): boolean {
-    let sorted = nums.sort((a, b) => a - b)
-    for(let i = 0; i < sorted.length-1; i++) {
-        if(sorted[i] === sorted[i+1]) {
+    //create a new set, because set value is unique
+    let temp = new Set<number>()
+
+    //iterate nums, check wether the temp has the current value or not
+    //if not, then add the current value to the set
+    //if it is, return true, because there's duplicate
+    for(let i=0; i<nums.length; i++) {
+        if(temp.has(nums[i])) {
             return true
-        } 
+        }
+        temp.add(nums[i])
     }
     return false
 };
