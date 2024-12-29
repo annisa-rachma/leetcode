60e48c86678b5d8e13e18de9255968bff23399eb
Annisa Rachmasari <saa.annisa15@gmail.com>
2024-12-30 06:20:39 +0700

    Time: 5 ms (55.66%), Space: 53.1 MB (29.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,17 +1,15 @@
 function twoSum(nums: number[], target: number): number[] {
-    let numMap:{[key:number] : number} = {}
+    let numMap = new Map()
     //iterate, make a difference variabel
     //check wether the difference exist in the map
     //if not, store the current num and its index on map
     //it it is, return the current index and the index on the map
     for(let i=0; i<nums.length; i++) {
         let difference = target - nums[i]
-        for(let key in numMap) {
-            if(parseInt(key) === difference) {
-                return [numMap[key], i]
-            }
+        if(numMap.has(difference)) {
+            return [numMap.get(difference), i]
         }
-        numMap[nums[i]] = i
+        numMap.set(nums[i], i)
     }
     return []
 };
